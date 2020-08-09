59f4f413cf4a37d7b32720f53d58825e4970a40b
Roberto Arevalo <roberttt44@hotmail.com>
2020-08-09 01:14:44 -0700

    aa

--- What is a Restful API?.py
+++ What is a Restful API?.py
@@ -1,1 +1,9 @@
 #API
+
+def raise_to_power(base_num, pow_num): #This is a function with 2 inputs. base number and pow number.
+    result = 1 #it will result the math
+    for index in range(pow_num): #it will loop through this for loop as many times as is specified in pow_num
+        result = result * base_num #multiplies the result by base_num
+    return result # calls the result to be returned
+
+print(raise_to_power(3, 4))
